93fd19b17d7bb3b60748a5ce996f60404e3a982d
Zach-Fisherman <zacharie.poissonnier@gmail.com>
2019-12-02 13:53:06 +0100

    base du plateau

---
+++ src/Controller/PublicController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PublicController extends AbstractController
+{
+    /**
+     * @Route("/", name="public")
+     */
+    public function index()
+    {
+        return $this->render('public/index.html.twig', [
+        ]);
+    }
+}
